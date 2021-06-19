a9a0df3a8428d280876dcb94361cfb19bf424819
Ayelen Guini <ayelen.guini@gmail.com>
2021-06-19 18:05:43 -0300

    Adds Styled Components and Colors enum

---
+++ client/src/style/Colors.ts
@@ -0,0 +1,9 @@
+export enum Colors {
+    CORAL = '#f9b4ab',
+    SAND = '#fdebd3',
+    BLUE = '#264e70',
+    GREEN = '#679186',
+    LIGHT_GREEN = '#bbd4ce',
+    LIGHT_GREEN_60 = 'rgba(187, 212, 206, 0.62)',
+    WHITE = '#FFFFFF',
+}
