a12bc026c985eed7b38f590e91097bcb276f1e95
Ayelen Guini <ayelen.guini@gmail.com>
2021-06-19 15:30:27 -0300

    Connects API with bff

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 var cors = require('cors');
+const { default: axios } = require('axios');
 
 const app = express();
 app.use(cors());
@@ -9,14 +11,18 @@ const PORT = process.env.PORT || 8080;
 // This is a HTTP request logger
 app.use(morgan('tiny'));
 
-// Define our routes
-app.get('/api', (req, res) => {
-  const data = {
-    username: 'ayelenmarie',
-    type: 'cool',
-  };
+// API URLs to use
+const forecastURL = `https://api.openweathermap.org/data/2.5/onecall?`;
 
-  res.json(data);
+// Define our route
+app.get('/forecast/:lat/:lon', function (req, res) {
+  axios
+    .get(
+      `${forecastURL}&lat=${req.params.lat}&lon=${req.params.lon}&exclude=minutely,hourly,alerts&appid=${process.env.WEATHER_KEY}`
+    )
+    .then((response) => res.json(response.data))
+    .then((forecast) => res.send(forecast))
+    .catch((error) => res.send(error));
 });
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
