1908e08de02248f9a33e1f1f766e9c71a5a6c57f
Ayelen Guini <ayelen.guini@gmail.com>
2021-06-25 19:08:34 -0300

    Adds weather icons

--- client/src/components/CurrentForecast.tsx
+++ client/src/components/CurrentForecast.tsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
-import _ from 'lodash'
-import { format } from 'prettier'
 import { FC } from 'react'
 import styled from 'styled-components'
+import { format } from 'date-fns'
+
 import { Colors } from '../style/Colors'
 import { CurrentForecast as CurrentForecastType } from '../types/Forecast'
+import cloud from '../images/cloud.png'
 
 /*
  * Types
  */
 
-type CurrentForecastProps = {
+export type CurrentForecastProps = {
     currentForecast: CurrentForecastType
 }
 
@@ -19,12 +20,76 @@ type CurrentForecastProps = {
  */
 
 const Container = styled.div`
-    padding: 5px;
+    padding: 10px;
+    background-color: ${Colors.WHITE};
+    border-radius: 10px 0px 0px 10px;
+    grid-row-start: 1;
+    grid-row-end: 3;
+`
+
+const VerticalTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px 0px 5px 0px;
+`
+
+const DateText = styled.p`
+    color: ${Colors.GREY};
+    font-size: 15px;
+`
+
+const TemperatureContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+`
+
+const Temperature = styled.p`
+    font-weight: 300;
+    font-size: 200px;
+    margin: 0px;
+    color: ${Colors.BLUE};
+`
+
+const TemperatureUnit = styled.p`
+    padding-top: 45px;
+    font-weight: 300;
+    font-size: 25px;
+    margin: 0px;
+    color: ${Colors.BLUE};
 `
 
 const Text = styled.p`
-    color: ${Colors.CORAL};
-    font-size: 20px;
+    margin: 0px;
+    font-size: 18px;
+    color: ${Colors.GREY};
+`
+
+const ItalicText = styled.p`
+    margin: 0px;
+    margin-left: 5px;
+    font-size: 18px;
+    font-style: italic;
+`
+
+const Image = styled.img`
+    height: 80px;
+    width: 80px;
+    margin-left: 8px;
+    margin-right: 5px;
+`
+
+const WeatherImage = styled.img`
+    height: 90px;
+    width: 90px;
+`
+
+const Separator = styled.div`
+    margin: 20px 0px 20px 0px;
+    height: 1px;
+    width: 100%;
+    background-color: ${Colors.OFF_WHITE};
 `
 
 /*
@@ -34,16 +99,30 @@ const Text = styled.p`
 export const CurrentForecast: FC<CurrentForecastProps> = ({
     currentForecast,
 }) => {
-    const { temperature, date, weather } = currentForecast || {}
-    const dateString = new Date(date).toLocaleDateString()
+    const { temperature, date, weather, feelsLike, clouds } = currentForecast
+    const forecastDate = new Date(date)
+    const forecastIcon =
+        `http://openweathermap.org/img/wn/${weather.icon}@2x.png` || null
 
-    console.log('date string', dateString)
+    const formattedDate = format(forecastDate, 'cccc, PPp ')
 
     return (
         <Container>
-            <Text>{`Date: ${dateString}`}</Text>
-            <Text>{temperature}</Text>
-            <Text>{weather.title}</Text>
+            <DateText>{formattedDate}</DateText>
+            <TemperatureContainer>
+                <Temperature>{Math.round(temperature)}</Temperature>
+                <TemperatureUnit>°C</TemperatureUnit>
+            </TemperatureContainer>
+            <ItalicText>Feels like {Math.round(feelsLike)}°</ItalicText>
+            <Separator />
+            <VerticalTextContainer>
+                {forecastIcon && <WeatherImage src={forecastIcon} />}
+                <Text>{weather.description}</Text>
+            </VerticalTextContainer>
+            <VerticalTextContainer>
+                <Image src={cloud} />
+                <Text>{clouds}%</Text>
+            </VerticalTextContainer>
         </Container>
     )
 }
