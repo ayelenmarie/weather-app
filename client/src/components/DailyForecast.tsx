d957eaa48b650fc3f074e37af88617300e4848eb
Ayelen Guini <ayelen.guini@gmail.com>
2021-06-25 19:09:09 -0300

    Adds DailyForecast component

---
+++ client/src/components/DailyForecast.tsx
@@ -0,0 +1,105 @@
+/* eslint-disable react/prop-types */
+import { FC } from 'react'
+import styled from 'styled-components'
+import { format } from 'date-fns'
+
+import { Colors } from '../style/Colors'
+import { DailyForecast as DailyForecastType } from '../types/Forecast'
+
+/*
+ * Types
+ */
+
+type DailyForecastProps = {
+    dailyForecast: DailyForecastType[]
+}
+
+type DailyForecastItemProps = {
+    item: DailyForecastType
+}
+
+/*
+ * Styles
+ */
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`
+
+const DateText = styled.p`
+    margin: 0px;
+    color: ${Colors.BLUE};
+    font-size: 15px;
+`
+
+const ItemContainer = styled.div`
+    height: 90px;
+    width: 80px;
+    margin: 10px;
+    padding: 10px 5px 10px 5px;
+    background-color: ${Colors.WHITE};
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const WeatherImage = styled.img`
+    height: 40px;
+    width: 40px;
+`
+
+const TemperatureContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+const Max = styled.p`
+    margin: 0px;
+    font-size: 15px;
+    margin-right: 5px;
+`
+
+const Min = styled.p`
+    margin: 0px;
+    font-size: 15px;
+    color: ${Colors.GREY};
+`
+
+const DailyForecastItem = ({ item }: DailyForecastItemProps) => {
+    const { date, weather, temperature } = item
+
+    const forecastDate = new Date(date)
+    const formattedDate = format(forecastDate, 'ccc')
+    const forecastIcon =
+        `http://openweathermap.org/img/wn/${weather?.icon}@2x.png` || null
+
+    return (
+        <ItemContainer>
+            <DateText>{formattedDate}</DateText>
+            {forecastIcon && <WeatherImage src={forecastIcon} />}
+            <TemperatureContainer>
+                <Max>{Math.round(temperature.maximum)}°</Max>
+                <Min>{Math.round(temperature.minimum)}°</Min>
+            </TemperatureContainer>
+        </ItemContainer>
+    )
+}
+
+/*
+ * DailyForecast
+ */
+
+export const DailyForecast: FC<DailyForecastProps> = ({ dailyForecast }) => {
+    return (
+        <Container>
+            {dailyForecast?.map((day, index) => {
+                return <DailyForecastItem item={day} key={index} />
+            })}
+        </Container>
+    )
+}
--- client/src/style/Colors.ts
+++ client/src/style/Colors.ts
@@ -6,4 +6,6 @@ export enum Colors {
     LIGHT_GREEN = '#bbd4ce',
     LIGHT_GREEN_60 = 'rgba(187, 212, 206, 0.62)',
     WHITE = '#FFFFFF',
+    OFF_WHITE = '#F6F6F8',
+    GREY = '#D6D7DA',
 }
